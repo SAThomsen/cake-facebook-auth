6ec642d113bbb596894bdb9357d1be678010010a
Mesterjagel <sebastian.thomsen@gmail.com>
2017-02-15 18:37:20 +0100

    Added tests

--- tests/Auth/FacebookAuthenticationTest.php
+++ tests/Auth/FacebookAuthenticationTest.php
@@ -17,4 +17,95 @@ class FacebookAuthenticateTest extends TestCase
         'plugin.SAThomsen\FacebookAuth.users',
         'plugin.SAThomsen\FacebookAuth.social_profiles',
     ];
+
+    /**
+     * Facebook token
+     *
+     * @var string
+     **/
+    protected $_token;
+
+
+    /**
+     * Setup general environment
+     *
+     * @return void
+     */
+    public function setUp() 
+    {
+        parent::setUp();
+
+        Security::salt('secret-key');
+        $this->Registry = $this->createMock('Cake\Controller\ComponentRegistry');
+        $this->response = $this->createMock('Cake\Network\Response');
+        $this->Auth = new FacebookAuthenticate($this->Registry, []);
+
+        // Configure facebook
+        Configure::write('facebook.appId', 'APP_ID');
+        Configure::write('facebook.appSecret', 'APP_SECRET');
+        Configure::write('facebook.graphVersion', 'v2.8');
+        Configure::write('facebook.fields', 'id,name,first_name,middle_name,last_name,gender,email');
+
+        $this->_token = "FACEBOOK_TOKEN";
+    }
+
+     /**
+      * @test
+      */
+     public function getUserViaTokenInHeader()
+     {
+        $request = new Request('posts/index');
+        $request->env('HTTP_AUTHORIZATION', 'facebook ' . $this->_token);
+        $result = $this->Auth->getUser($request, $this->response);
+
+        $expected = [
+            'id' => 1,
+            'active' => true
+        ];
+
+        $this->assertEquals($expected, $result);
+     }
+
+     /**
+      * @test
+      */
+     public function getUserViaTokenInQueryParam()
+     {
+        $qParam = 'facebook';
+        $this->Auth = new FacebookAuthenticate($this->Registry, ['parameter' => $qParam ]);
+        $request = new Request('posts/index?'. $qParam . '=' . $this->_token);
+        $result = $this->Auth->getUser($request, $this->response);
+
+        $expected = [
+            'id' => 1,
+            'active' => true
+        ];
+
+        $this->assertEquals($expected, $result);
+     }
+
+     /**
+      * @test
+      */
+     public function getUserQueryParameterNotSet()
+     {
+        $qParam = 'facebook';
+        $this->Auth = new FacebookAuthenticate($this->Registry, []);
+        $request = new Request('posts/index?'. $qParam . '=' . $this->_token);
+        $result = $this->Auth->getUser($request, $this->response);
+
+        $this->assertFalse($result);
+     }
+
+     /**
+      * @test
+      */
+     public function getUserNoToken()
+     {
+        $this->Auth = new FacebookAuthenticate($this->Registry, []);
+        $request = new Request('posts/index?');
+        $result = $this->Auth->getUser($request, $this->response);
+
+        $this->assertFalse($result);
+     }
 }
